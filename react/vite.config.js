4d7b8f883fd4ec1906b1e4429f5216ca8e664291
Anna <kotlyaranya1771@gmail.com>
2023-12-10 21:35:38 +0200

    --- config ---

--- react/vite.config.js
+++ react/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/react",
+  base: "/RentCar/react",
   resolve: {
     alias: { src: "/src" },
   },
