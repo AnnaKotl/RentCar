3f3e28aa1a25f28b004ac61807511d7b7141070d
Анна Котляр <123831146+AnnaKotl@users.noreply.github.com>
2024-12-06 13:55:17 +0200

    routs & render

--- src/components/Navigation/NavLogo.jsx
+++ src/components/Navigation/NavLogo.jsx
@@ -1,14 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import carLogo from "/images/nav-logo.png";
 import { Logo } from "./NavHeader.styled";
 
 const NavLogo = () => {
   return (
-    <>
-      <a href="/">
-        <Logo src={carLogo} alt="car-logo" height="80px" />
-      </a>
-    </>
+    <Link to="/">
+      <Logo src={carLogo} alt="car-logo" height="80px" />
+    </Link>
   );
 };
 
